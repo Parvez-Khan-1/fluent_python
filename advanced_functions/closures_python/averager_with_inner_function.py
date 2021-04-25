f1017cf07a935d81db2bc3e9798689a8c4bf1fce
unknown <parvezpathan09@gmail.com>
2021-04-26 00:27:13 +0530

    Use of non-local keyword in python

---
+++ advanced_functions/closures_python/averager_with_inner_function.py
@@ -0,0 +1,16 @@
+# Continues average with nested function
+
+def averager():
+    series = []
+
+    def average(new_value):
+        series.append(new_value)  # We are able to append it because lists are mutable
+        total = sum(series)
+        return total/len(series)
+
+    return average
+
+avg = averager()
+print(avg(10))
+print(avg(15))
+print(avg(20))
---
+++ advanced_functions/closures_python/averager_without_keeping_all_history.py
@@ -0,0 +1,17 @@
+# Using Local Variable
+
+def averager():
+    count = 0
+    sum = 0
+    def average(num):
+        nonlocal count, sum
+        count +=1
+        sum += num
+        return sum/count
+    return average
+
+
+avg = averager()
+print(avg(10))
+print(avg(15))
+print(avg(20))
