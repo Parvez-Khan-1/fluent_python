305c07e3d922ad8adaabef52ce6138dd36f5b4e6
unknown <parvezpathan09@gmail.com>
2021-05-16 00:49:28 +0530

    Multiple Inteheritance In Python

---
+++ oop_python/multiple_inheritance.py
@@ -0,0 +1,23 @@
+
+class A:
+    def hello(self):
+        print("Hello Class A")
+
+
+class B:
+    def hello(self):
+        print("Hello Class B")
+
+
+class C(A, B):
+    def hello(self):
+        super().hello()
+
+
+c = C()
+c.hello()  # The python Method Resolution Order calls the hello function of Class A
+
+# If we want to call hello func of class B we can call it as below:
+# class C(A, B):
+#     def hello(self):
+#         super().hello()
